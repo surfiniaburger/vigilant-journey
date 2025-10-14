0be71e00f82bfd3a3b27475fe3c224505a76e233
surfiniaburger <jdmasciano2@gmail.com>
2025-10-14 23:15:59 +0000

    made changes based on recommendations

--- mooncake/__mocks__/streamdown.tsx
+++ mooncake/__mocks__/streamdown.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const StreamDown = ({ children }: { children: React.ReactNode }) => {
+export const Streamdown = ({ children }: { children: React.ReactNode }) => {
   return <div>{children}</div>;
 };
-
-export default StreamDown;
--- mooncake/components/ui/response.tsx
+++ mooncake/components/ui/response.tsx
@@ -1,8 +1,21 @@
-import { memo } from "react"
+"use client"
+
+import { memo, type ComponentProps } from "react"
+import { Streamdown } from "streamdown"
+
+import { cn } from "@/lib/utils"
+
+type ResponseProps = ComponentProps<typeof Streamdown>
 
 export const Response = memo(
-  ({ children }: { children: React.ReactNode }) => (
-    <div>{children}</div>
+  ({ className, ...props }: ResponseProps) => (
+    <Streamdown
+      className={cn(
+        "size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0",
+        className
+      )}
+      {...props}
+    />
   ),
   (prevProps, nextProps) => prevProps.children === nextProps.children
 )
